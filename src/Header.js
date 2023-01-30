8ff57013b9bea899fc52f6323298f62bfc75654e
khalteck <khalidoyeneye@gmail.com>
2023-01-30 05:43:36 +0100

    build

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <img alt="" src="/images/logo.png" className="w-[150px] h-auto" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
